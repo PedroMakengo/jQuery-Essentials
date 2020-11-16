1280bd05add9fbd0005e39b42999bfc54c78e427
Pedro Makengo <pedromakengomiguel@gmail.com>
2020-11-16 14:39:24 +0100

    Modulo 2

---
+++ 02-seletores/02-hierarquia.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hierarquia | jQuery Essentials | UpInside</title>
+</head>
+<body>
+
+  
+    <!-- jQuery Essentials -->
+    
+    <script src="../js/jquery.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
