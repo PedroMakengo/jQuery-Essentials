2ff47f7761d09efa23ae67af7c3aa91257bd849f
Pedro Makengo <pedromakengomiguel@gmail.com>
2020-11-18 11:15:51 +0100

    Filtros básicos

---
+++ 02-seletores/03-filtros-basicos.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filtros Básicos | jQuery Essentials | UpInside</title>
+    <style>
+        p {
+            margin: 0;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+
+    
+
+    <!-- jQuery Essentials -->
+    <div class="list" style="padding: 10%">
+        <input type="text" name="01" placeholder="Sem foco" />
+        <input type="text" name="01" placeholder="Com foco" autofocus />
+        <h1>Minha lista zebrada <span></span></h1>
+        <p>P 01</p>
+        <p>P 02</p>
+        <p class="p">P 03</p>
+        <p>P 04</p>
+        <p>P 05</p>
+        <h2>Elementos GT <span></span></h2>
+        <p class="p">P 06</p>
+        <p>P 07</p>
+        <p>P 08</p>
+        <p>P 09</p>
+        <p>P 10</p>
+    </div>
+    
+    <script src="../js/jquery.js"></script>
+    <script src="filtros-basicos.js"></script>
+</body>
+</html>
---
+++ 02-seletores/filtros-basicos.js
@@ -0,0 +1,49 @@
+$(function () {
+    // Pegando todos os elementos que não possuem uma classe
+    $("p:not('.p')").css("font-weight", "bold");
+
+    // Pegando o primeiro elemento
+    $("p:first").css("color", "red");
+
+    // Pegando o último elemento
+    $("p:last").css("color", "red");
+
+    // Pegando os elementos pares
+    $("p:even").css("background", "#eee");
+
+    // Pegando os elementos impares 
+    $("p:odd").css("background", "#ccc");
+
+    // Pegando os elementos pelos os seus indíces
+    $("p:eq(4)").text("jQuery Essentials");
+
+    // Todos os elementos a cima da posição 4
+    $("p:gt(4)").text("Selectores GT");
+
+    // Pegando os sprimeiros elementos
+    $("p:lt(5)").text("Selectores LT");
+
+
+    $(".list :header").css({
+        "font-size": "1.4em",
+        color: "#888"
+    });
+
+    $(".list :header span").css("color", "blue");
+
+    function animar_el(elemento) {
+        $(elemento).fadeToggle(400, function () {
+            animar_el(elemento);
+        });
+    }
+
+    var num = 1;
+    setInterval(function () {
+        $(":animated").text(num);
+        num++;
+    }, 800);
+    animar_el(".list span");
+
+
+    $(":focus").css("padding", "10px");
+});
